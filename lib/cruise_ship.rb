413a083802a164f1b8e0e4adb1a7577dd809da46
Kevin W <kcwang06@gmail.com>
2019-04-16 06:34:07 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,10 +6,10 @@
 # suite_e: "Crumpet the Elf"
 # }
 passengers = {
-suite_a: "Amanda Presley", 
-suite_b: "Seymour Hoffman", 
-suite_c: "Alfred Tennyson", 
-suite_d: "Charlie Chaplin", 
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
 suite_e: "Crumpet the Elf"
 }
 
